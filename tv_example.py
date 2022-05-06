2ef197baecd1a72d7b1c439dd0091b08cf9f2aed
torstenvolk <torsten.volk@yahoo.com>
2022-05-06 12:39:42 -0600

    Update tv_example.py

--- tv_example.py
+++ tv_example.py
@@ -55,14 +55,14 @@ def main():
     data_only = json_response["data"]
     data_only1 = json_response1["data"]
     df = pd.DataFrame(data_only)
-
+	df1 = pd.DataFrame(data_only)
     
     df["created_at"] = pd.to_datetime(df["created_at"])
     df["created_at"] = df["created_at"].dt.strftime("%Y-%m-%d %H:%M:%S")
 
     df_show = df.set_index("created_at", inplace=False)
     st.table(df_show['text'])
-	
+
 	#json_response1 = connect_to_endpoint(metrics_url, metrics_query_params)
     #
     #st.write(df1)
