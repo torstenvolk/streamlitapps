5ada95c8283b355e2b91030fc6a96e2ee322cff2
torstenvolk <torsten.volk@yahoo.com>
2022-05-06 12:22:09 -0600

    Update tv_example.py

--- tv_example.py
+++ tv_example.py
@@ -56,7 +56,7 @@ def main():
 
     df_show = df.set_index("created_at", inplace=False)
     st.table(df_show['text'])
-	st.write(json_response1)
+
 	#json_response1 = connect_to_endpoint(metrics_url, metrics_query_params)
     #
     #st.write(df1)
