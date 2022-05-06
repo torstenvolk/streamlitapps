bc2d64f337312df44f1abcb8ea84c7a610256f0d
torstenvolk <torsten.volk@yahoo.com>
2022-05-06 12:00:09 -0600

    Update tv_example.py

--- tv_example.py
+++ tv_example.py
@@ -21,7 +21,7 @@ metrics_url = "https://api.twitter.com/2/tweets/counts/recent"
 
 
 query_params = {'query':st.text_input('query', 'ema_research'), 'max_results':100,  'tweet.fields':'created_at,public_metrics'}
-metrics_query_params = {'query':query}
+metrics_query_params = {'query':'query'}
 
 
 ##########################################
