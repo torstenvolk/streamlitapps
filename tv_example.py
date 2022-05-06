ac2e2ef5586186029a35c6bd56ca2c6165b9bb0c
torstenvolk <torsten.volk@yahoo.com>
2022-05-06 11:57:51 -0600

    Update tv_example.py

--- tv_example.py
+++ tv_example.py
@@ -56,9 +56,9 @@ def main():
     df_show = df.set_index("created_at", inplace=False)
     st.table(df_show['text'])
 
-	json_response1 = connect_to_endpoint(metrics_url, metrics_query_params)
-    df1 = pd.DataFrame(json_response1)
-    st.write(df1)
+	#json_response1 = connect_to_endpoint(metrics_url, metrics_query_params)
+    #df1 = pd.DataFrame(json_response1)
+    #st.write(df1)
 
 if  __name__ == "__main__":
     main()
