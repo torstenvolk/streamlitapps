245584853ea050f0b37e74d93c1704a7aecc19a4
torstenvolk <torsten.volk@yahoo.com>
2022-05-08 14:24:05 -0600

    Update tv_example.py

--- tv_example.py
+++ tv_example.py
@@ -1,14 +1,14 @@
-import streamlit
 from requests.api import request
 import streamlit as st
 import streamlit.components.v1 as components
-import requests
 import requests
 import os
 import json
 import numpy as np
 import pandas as pd
 import time
+import streamlit.components.v1 as components
+
 
 ################# Variables ###########
 bearer_token = st.secrets["bearer_token"]
@@ -56,6 +56,31 @@ def main():
     df_show = df.set_index("created_at", inplace=False)
     st.table(df_show['text'])
 	
+
+
+
+class Tweet(object):
+    def __init__(self, s, embed_str=False):
+        if not embed_str:
+            # Use Twitter's oEmbed API
+            # https://dev.twitter.com/web/embedded-tweets
+            api = "https://publish.twitter.com/oembed?url={}".format(s)
+            response = requests.get(api)
+            self.text = response.json()["html"]
+        else:
+            self.text = s
+
+    def _repr_html_(self):
+        return self.text
+
+    def component(self):
+        return components.html(self.text, height=600)
+
+
+t = Tweet("https://twitter.com/OReillyMedia/status/901048172738482176").component()
+
+
+
 	
 if  __name__ == "__main__":
     main()
