08e1e881d685c5beee410b5a7c441ea21c10522b
torstenvolk <torsten.volk@yahoo.com>
2022-05-08 15:45:21 -0600

    Update tv_example.py

--- tv_example.py
+++ tv_example.py
@@ -53,6 +53,7 @@ def main():
     df["created_at"] = df["created_at"].dt.strftime("%Y-%m-%d %H:%M:%S")
     df_show = df.set_index("created_at", inplace=False)
     st.table(df_show[['text','id']])
+	return df_show
 
 def theTweet(tweet_url):
 	api = "https://publish.twitter.com/oembed?url={}".format(tweet_url)
