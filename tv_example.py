5d54ed51e51e859c258a07c1008b8ff159f682be
torstenvolk <torsten.volk@yahoo.com>
2022-05-09 10:21:27 -0600

    Update tv_example.py

--- tv_example.py
+++ tv_example.py
@@ -49,7 +49,7 @@ def main():
     df["created_at"] = pd.to_datetime(df["created_at"])
     df["created_at"] = df["created_at"].dt.strftime("%Y-%m-%d %H:%M:%S")
     df_show = df.set_index("created_at", inplace=False)
-    st.table(df_show["text"])
+    st.table(df_show)
 
 	
 if  __name__ == "__main__":
