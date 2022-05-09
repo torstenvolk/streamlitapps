5a7f8ae4e77dbbe0e0e2190f1987c585d0e99f8a
torstenvolk <torsten.volk@yahoo.com>
2022-05-09 10:10:23 -0600

    Update tv_example.py

--- tv_example.py
+++ tv_example.py
@@ -21,10 +21,7 @@ search_url = "https://api.twitter.com/2/tweets/search/recent"
 # Optional params: start_time,end_time,since_id,until_id,max_results,next_token,
 # expansions,tweet.fields,media.fields,poll.fields,place.fields,user.fields
 
-query = st.text_input('query', 'kubernetes')
 query_params = {'query':st.text_input('query', 'ema_research'), 'max_results':100,   'tweet.fields':'created_at,id'}
-st.write(query)	
-
 
 
 ##########################################
@@ -38,8 +35,7 @@ def bearer_oauth(r):
     r.headers["User-Agent"] = "v2RecentSearchPython"
     return r
 
-def connect_to_endpoint(url, params):
-    
+def connect_to_endpoint(url, params):    
     response = requests.get(url, auth=bearer_oauth, params=params)
     print(response.status_code)
     if response.status_code != 200:
