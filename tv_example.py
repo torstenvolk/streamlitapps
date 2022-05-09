78323535be65dceb0b7d5514cc73e26ed259637b
torstenvolk <torsten.volk@yahoo.com>
2022-05-09 10:20:44 -0600

    Update tv_example.py

--- tv_example.py
+++ tv_example.py
@@ -21,7 +21,7 @@ search_url = "https://api.twitter.com/2/tweets/search/recent"
 # Optional params: start_time,end_time,since_id,until_id,max_results,next_token,
 # expansions,tweet.fields,media.fields,poll.fields,place.fields,user.fields
 
-query_params = {'query':st.text_input('query', 'ema_research'), 'max_results':100,   'tweet.fields':'created_at,id,public_metrics'}
+query_params = {'query':st.text_input('query', 'ema_research'), 'max_results':100,   'tweet.fields':'created_at,id'}
 
 
 ##########################################
